431796335acf4648f91103a679514386782f91d7
Breyner Mola <breyner.mola.9@gmail.com>
2018-03-08 19:37:29 -0500

    php mvc

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "driver"    =>"mysql",
+    "host"      =>"localhost",
+    "user"      =>"root",
+    "pass"      =>"12345",
+    "database"  =>"pruebas",
+    "charset"   =>"utf8"
+);
+?>
